0df65897510a798b98278509bfa28b1dca182ade
Mike S <CyberMerln@gmail.com>
2019-09-01 15:35:11 -0400

    Updated ScrollTest.ino to use framebuffer

--- examples/scrollTest/scrollTest.ino
+++ examples/scrollTest/scrollTest.ino
@@ -19,15 +19,15 @@
 #include <SPI.h>
 #include <st7735_t3_font_ComicSansMS.h>
 
-#define TFT_MISO  1
-#define TFT_MOSI  26  //a12
-#define TFT_SCK   27  //a13
-#define TFT_DC   6 
-#define TFT_CS   0  
-#define TFT_RST  5
+#define TFT_MISO  12
+#define TFT_MOSI  11  //a12
+#define TFT_SCK   13  //a13
+#define TFT_DC   9 
+#define TFT_CS   10  
+#define TFT_RST  8
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-ST7789_t3 tft = ST7789_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST, TFT_MISO);
+ST7789_t3 tft = ST7789_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
 
 // If using the breakout, change pins as desired
 //Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
@@ -36,10 +36,10 @@ void setup() {
 
   Serial.begin(9600);
 
-
  
   tft.init(240, 320);           // Init ST7789 320x240
   tft.setRotation(3);
+  tft.useFrameBuffer(true);
 
   tft.fillScreen(ST7735_BLACK);
   while (!Serial) ; 
@@ -61,23 +61,26 @@ void setup() {
     tft.print("  this is line ");
     tft.println(i);
     delay(100);
+    tft.updateScreen();
   }
 
   tft.fillScreen(ST7735_BLACK);
   tft.setScrollTextArea(40,50,120,120);
+
   tft.setScrollBackgroundColor(ST7735_GREEN);
+
   tft.setFont(ComicSansMS_10);
 
   tft.setTextSize(1);
   tft.setCursor(40, 50);
 
   for(int i=0;i<20;i++){
-    tft.print("  this is line ");
-    tft.println(i);
+    tft.printf("  this is line ");
+    tft.printf("%d\n",i);
+    tft.updateScreen();
     delay(500);
   }
-
-
+    tft.updateScreen();
 }
 
 
